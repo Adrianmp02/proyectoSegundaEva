6203b480c206edfb69bf972a51402cdaaa5d429a
Adrianmp02 <adrian.marper.6@educa.jcyl.es>
2024-02-28 12:47:16 +0100

    Asiento subclass

---
+++ Avioncito/src/avion/AsientoLibre.java
@@ -0,0 +1,11 @@
+package avion;
+
+public class AsientoLibre extends Asiento {
+
+	public AsientoLibre(char letra, int fila) {
+		super(letra, fila);
+		// TODO Auto-generated constructor stub
+		
+	}
+
+}
