73481155c808c8a71b3c22295a7559106f565cfc
dic43 <dic43@PortatilDavid>
2024-02-29 19:35:38 +0100

    menu

--- Avioncito/src/avion/Menu.java
+++ Avioncito/src/avion/Menu.java
@@ -7,84 +7,75 @@ import java.util.Scanner;
 
 public class Menu implements MainMenuInterface {
 
-	public int scannerInt() {
-
-		@SuppressWarnings("resource")
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-
-		return n;
-
-	}
+	
 
 	@Override
 	public void iniciar() {	
-		int eleccion= scannerInt();
 
 		StringBuffer str = new StringBuffer();
 
-		str.append("°°°°°°°°°°°°°°°°°°°°°°°°°°°");
-		str.append("\n||                       ||");
-		str.append("\n||   ELIJA UNA OPCIÒN:   ||");
+		str.append("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+		str.append("\n||                              ||");
+		str.append("\n||      ELIJA UNA OPCIÒN:       ||");
 		str.append("\n");
-		str.append("||-----------------------||");
-		str.append("\n||                       ||");
+		str.append("||------------------------------||");
+		str.append("\n||                              ||");
 		str.append("\n");
-		str.append("|| １ ► Billete Suelto");
-		str.append("\uD83C\uDF40┊"); 
-		str.append(" ||");
-		str.append("\n||                       ||");
-		str.append("\n||                       ||");
+		str.append("|| 1 ► Billete Suelto");
+		 
+		str.append("           ||");
+		str.append("\n||                              ||");
+		str.append("\n||                              ||");
 		str.append("\n");
-		str.append("|| ２ ► Varios billetes");  
-		str.append("\uD83D\uDCBE┊");
-		str.append(" ||");
-		str.append("\n||                       ||");
-		str.append("\n||                       ||");
+		str.append("|| 2 ► Varios billetes");  
+		
+		str.append("          ||");
+		str.append("\n||                              ||");
+		str.append("\n||                              ||");
 		str.append("\n");
-		str.append("|| ３ ► Comprobación libre/ocupado        ");  
-		str.append("\uD83D\uDEAA┊");
-		str.append(" ||");
-		str.append("\n||                       ||");
-		str.append("\n||                       ||");
-		str.append("|| 4 ► Ubicación Asiento");  
-		str.append("\uD83D\uDEAA┊");
-		str.append(" ||");
-		str.append("\n||                       ||");
-		str.append("\n||                       ||");
-		str.append("|| 5 ► Salir        ");  
-		str.append("\uD83D\uDEAA┊");
-		str.append(" ||");
-		str.append("\n||                       ||");
-		str.append("\n||                       ||");
-		str.append("\n°°°°°°°°°°°°°°°°°°°°°°°°°°°");
-
+		str.append("|| 3 ► Asiento libre/ocupado    ");  
+		
+		str.append("||");
+		str.append("\n||                              ||");
+		str.append("\n||                              ||");
+		str.append("\n|| 4 ► Ubicación Asiento");  
+		
+		str.append("        ||");
+		str.append("\n||                              ||");
+		str.append("\n||                              ||");
+		str.append("\n|| 5 ► Salir        ");  
+		
+		str.append("            ||");
+		str.append("\n||                              ||");
+		str.append("\n||                              ||");
+		str.append("\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+		
 		System.out.println(str.toString());
-
+		int eleccion= scannerInt();
 		if(eleccion==1) {
 			StringBuffer str1 = new StringBuffer();
 
 			str1.append("°°°°°°°°°°°°°°°°°°°°°°°°°°°");
 			str1.append("\n||                       ||");
-			str1.append("\n||   TIPO DE ASIENTO:   ||");
+			str1.append("\n||   TIPO DE ASIENTO:    ||");
 			str1.append("\n");
 			str1.append("||-----------------------||");
 			str1.append("\n||                       ||");
 			str1.append("\n");
-			str1.append("|| １ ► Primera Clase");
-			str1.append("\uD83C\uDF40┊"); 
-			str1.append(" ||");
+			str1.append("|| 1 ► Primera Clase");
+			
+			str1.append("     ||");
 			str1.append("\n||                       ||");
 			str1.append("\n||                       ||");
 			str1.append("\n");
-			str1.append("|| ２ ► Clase Turista");  
-			str1.append("\uD83D\uDCBE┊");
-			str1.append(" ||");
+			str1.append("|| 2 ► Clase Turista");  
+			
+			str1.append("     ||");
 			str1.append("\n||                       ||");
 			str1.append("\n||                       ||");
 			str1.append("\n");
 			str1.append("°°°°°°°°°°°°°°°°°°°°°°°°°°°");
-			System.out.println(str.toString());
+			System.out.println(str1.toString());
 
 			int eleccion11 = scannerInt();
 			if (eleccion11 ==1) {
@@ -98,29 +89,30 @@ public class Menu implements MainMenuInterface {
 			}
 		}
 		if(eleccion==2) {
+			
 			StringBuffer str1 = new StringBuffer();
 
 			str1.append("°°°°°°°°°°°°°°°°°°°°°°°°°°°");
 			str1.append("\n||                       ||");
-			str1.append("\n||   TIPO DE ASIENTO:   ||");
+			str1.append("\n||   TIPO DE ASIENTO:    ||");
 			str1.append("\n");
 			str1.append("||-----------------------||");
 			str1.append("\n||                       ||");
 			str1.append("\n");
-			str1.append("|| １ ► Primera Clase");
-			str1.append("\uD83C\uDF40┊"); 
-			str1.append(" ||");
+			str1.append("|| 1 ► Primera Clase");
+			
+			str1.append("     ||");
 			str1.append("\n||                       ||");
 			str1.append("\n||                       ||");
 			str1.append("\n");
-			str1.append("|| ２ ► Clase Turista");  
-			str1.append("\uD83D\uDCBE┊");
-			str1.append(" ||");
+			str1.append("|| 2 ► Clase Turista");  
+			
+			str1.append("     ||");
 			str1.append("\n||                       ||");
 			str1.append("\n||                       ||");
 			str1.append("\n");
 			str1.append("°°°°°°°°°°°°°°°°°°°°°°°°°°°");
-			System.out.println(str.toString());
+			System.out.println(str1.toString());
 
 			int eleccion12 = scannerInt();
 			if (eleccion12 ==1) {
@@ -168,6 +160,15 @@ public class Menu implements MainMenuInterface {
 	@Override
 	public void mostrarAsientos() {
 		// TODO Auto-generated method stub
+
+	}
+	public int scannerInt() {
+
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		return n;
 
 	}
 }
