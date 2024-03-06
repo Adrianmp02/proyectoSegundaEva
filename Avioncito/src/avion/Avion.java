285428a4f7d5e311eaa0f0f65d32c084d0cffa9d
DavidIglesias69 <david.iglcua@educa.jcyl.es>
2024-03-06 12:05:50 +0100

    SUBIR

--- Avioncito/src/avion/Avion.java
+++ Avioncito/src/avion/Avion.java
@@ -1,217 +1,374 @@
+
 package avion;
-
+ 
 import java.util.ArrayList;
+
 import java.util.Arrays;
+
 import java.util.Iterator;
-
+ 
 public class Avion implements AvionInterface {
+ 
+	private ArrayList<Integer> filaPrimeraClase;
 
-	private ArrayList<Integer> filaP;
-	private ArrayList<Integer> filaT;
+	private ArrayList<Integer> filaTurista;
+
 	private ArrayList<Character> letraTurista;
-	private ArrayList<Character> letraFirstClass;
-	private ArrayList<Asiento> asientosPrimera;
-	private ArrayList<Asiento> asientosTurista;
 
+	private ArrayList<Character> letraPrimeraClase;
 
+	protected ArrayList<Asiento> asientosPrimera;
+
+	protected ArrayList<Asiento> asientosTurista;
+
+	protected ArrayList<Asiento> avionAsientos = new ArrayList<Asiento>();
+ 
+ 
 	@Override
+
 	public void crearAvion() throws AvionException {
-
+ 
 		if(asientosPrimera != null) {
-			throw new AvionException("Ya se encuentra un avion creado");
-		}
 
+			throw new AvionException("Ya se encuentra un avion creado");
+
+		}
+ 
 		if(asientosTurista != null) {
+
 			throw new AvionException("Ya se encuentra un avion creado");
-		}
 
-		filaP = new ArrayList<Integer>();
-		filaT = new ArrayList<Integer>();
+		}
+ 
+		filaPrimeraClase = new ArrayList<Integer>();
+
+		filaTurista = new ArrayList<Integer>();
+
 		letraTurista = new ArrayList<Character>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F'));
-		letraFirstClass = new ArrayList<Character>(Arrays.asList('A', 'B', 'E', 'F'));
+
+		letraPrimeraClase = new ArrayList<Character>(Arrays.asList('A', 'B', 'E', 'F'));
+
 		asientosPrimera = new ArrayList<Asiento>();
+
 		asientosTurista = new ArrayList<Asiento>();
-
+ 
 		//Damos valor a la fila desde el numero 1 hasta el 4
+
 		for(int i = 1; i <= 4; i++) {
-			filaP.add(i);
-		}
 
+			filaPrimeraClase.add(i);
+
+		}
+ 
 		//Damos valor a la fila desde el numero 5 hasta el 33
+
 		for(int i = 0; i <= 33; i++) {
-			filaT.add(i);
-		}
 
+			filaTurista.add(i);
+
+		}
+ 
 		//Asigancion de asientos Primera Clase
-		for(int i = 0; i < letraFirstClass.size(); i++) {
+
+		for(int i = 0; i < letraPrimeraClase.size(); i++) {
+
 			for(int j = 0; j < 4; j++) {
-				Asiento aux = new AsientoLibre(""+filaP.get(j)+letraFirstClass.get(i), 50.99);
 
+				Asiento aux = new AsientoLibre(""+filaPrimeraClase.get(j)+letraPrimeraClase.get(i), 50.99);
+ 
 				asientosPrimera.add(aux);
-			}
-		}
 
+			}
+
+		}
+ 
 		//Asignacion de asientos Turista
+
 		for(int i = 0; i < letraTurista.size(); i++) {
-			for(int j = 5; j < filaT.size(); j++) {
-				Asiento aux = new AsientoLibre(""+filaT.get(j)+letraTurista.get(i), 25.99);
 
+			for(int j = 5; j < filaTurista.size(); j++) {
+
+				Asiento aux = new AsientoLibre(""+filaTurista.get(j)+letraTurista.get(i), 25.99);
+ 
 				asientosTurista.add(aux);
+
 			}
+
+		}
+ 
+		for(int i = 0; i < asientosPrimera.size(); i++) {
+
+			avionAsientos.add(asientosPrimera.get(i));
+
 		}
 
-		//System.out.println(asientosTurista);
+		for(int i = 0; i < asientosTurista.size(); i++) {
 
+			avionAsientos.add(asientosTurista.get(i));
 
+		}
+
+ 
 	}
-
+ 
 	@Override
+
 	public void mostrarAvion() throws AvionException {
-
+ 
 		int variableP = 0;
+
 		int tamañoP = 3;
-
+ 
 		int variableT = 0;
+
 		int tamañoT = 28;
-
+ 
 		if(asientosPrimera == null) {
+
 			throw new AvionException("No se puede mostrar el avion, no hay ningun avion creado");
-		}
 
+		}
+ 
 		System.out.print("A  ");
-
+ 
 		for(int j = variableP; j <= tamañoP; j++) {
-
+ 
 			if(asientosPrimera.get(j).reservado) {
+
 				System.out.print("[❌]");
+
 			}else {
+
 				System.out.print("[✔]");
+
 			}
-
+ 
 		}
-
+ 
 		System.out.print(" |");
-
+ 
 		System.out.print(" A  ");
-
+ 
 		for(int j = variableT; j <= tamañoT; j++) {
-
+ 
 			if(asientosTurista.get(j).reservado) {
+
 				System.out.print("[❌]");
+
 			}else {
+
 				System.out.print("[✔]");
+
 			}
-
+ 
 		}
-
+ 
 		//-----------------------------------------------------------------------------------//
-
+ 
 		variableP += 4;
+
 		tamañoP += 4;
-		
+
 		variableT += 29;
+
 		tamañoT += 29;
+ 
+		System.out.println();
+
+		System.out.print("B  ");
+ 
+		for(int j = variableP; j <= tamañoP; j++) {
+ 
+			if(asientosPrimera.get(j).reservado) {
+
+				System.out.print("[❌]");
+
+			}else {
+
+				System.out.print("[✔]");
+
+			}
+ 
+		}
+ 
+		System.out.print(" |");
+ 
+		System.out.print(" B  ");
+ 
+		for(int j = variableT; j <= tamañoT;j++) {
+ 
+			if(asientosTurista.get(j).reservado) {
+ 
+				System.out.print("[❌]");
+
+			}else {
+
+				System.out.print("[✔]");
+
+			}
+ 
+		}
 
 		System.out.println();
-		System.out.print("B  ");
 
-		for(int j = variableP; j <= tamañoP; j++) {
-
-			if(asientosPrimera.get(j).reservado) {
-				System.out.print("[❌]");
-			}else {
-				System.out.print("[✔]");
-			}
-
-		}
-
-		System.out.print(" |");
-
-		System.out.print(" B  ");
-
-		for(int j = variableT; j <= tamañoT;j++) {
-
-			if(asientosTurista.get(j).reservado) {
-
-				System.out.print("[❌]");
-			}else {
-				System.out.print("[✔]");
-			}
-
-		}
 	}
-
+ 
 	@Override
-	public void comprobarAsiento(String numAsiento) {
 
-		Asiento aux; //Objeto asiento aux para la busqueda
-		boolean flag = false; //Por defecto false, se entiende que no esta reservado aun
-		for(int i = 0; i < asientosPrimera.size(); i++) {
-			aux = asientosPrimera.get(i);
-			if(aux.getNumAsiento().equals(numAsiento)) {
-				if(aux.isReservado() == true) { 
-					flag = true; //Si el asiento esta reservado, la flag pasa a true
-				}
+	public void comprobarAsiento(String numAsiento) throws AsientoException {
+ 
+		boolean flag = true;
+
+		for(int i = 0; i < avionAsientos.size(); i++) {
+
+			if(!numAsiento.toUpperCase().equals(avionAsientos.get(i).getNumAsiento())) {
+
+				flag = false;
+
+			}else {
+
+				flag = true;
+
+				break;
+
 			}
+
 		}
 
 		if(!flag) {
-			System.out.println("El asiento: "+numAsiento+", esta libre.");
+
+			throw new AsientoException("Asiento no valido");
+
+		}
+
+		boolean flagReservar = false;
+
+		for(int i = 0; i < avionAsientos.size(); i++) {
+
+			if(numAsiento.toUpperCase().equals(avionAsientos.get(i).getNumAsiento())) {
+
+				if(avionAsientos.get(i).reservado) {
+
+					flagReservar = true;
+
+				}
+
+			}
+
+		}
+
+		if(flagReservar) {
+
+			System.out.println("El asiento: "+numAsiento+" no esta disponible");
+
 		}else {
-			System.out.println("El asiento: "+numAsiento+", esta ocupado.");
+
+			System.out.println("El asiento: "+numAsiento+" esta disponible");
+
 		}
 
 	}
-
+ 
 	@Override
+
 	public void reservarAsiento(String numAsiento, Persona comprador) throws AsientoException {
-
+ 
 		Asiento cambio = new AsientoOcupado(numAsiento, comprador);
-		Asiento aux; //Objeto asiento aux para la busqueda
+ 
+		boolean isFirstClass = false;
 
-		boolean PoT = false;
-		
-		int j = 0;
-		for (int i = 0; i < asientosPrimera.size(); i++) {
-			if(!asientosPrimera.get(i).getNumAsiento().equals(numAsiento.toUpperCase())) {
-				j++;
+		boolean flagAsientoValido = true;
+
+		for(int i = 0; i < avionAsientos.size(); i++) {
+
+			if(!numAsiento.toUpperCase().equals(avionAsientos.get(i).getNumAsiento())) {
+
+				flagAsientoValido = false;
+
+			}else {
+
+				flagAsientoValido = true;
+
+				break;
+
 			}
+
 		}
 
-		if(j == 190) {
-			throw new AsientoException("Asiento introducido no valido");
+		if(!flagAsientoValido) {
+
+			throw new AsientoException("Asiento no valido");
+
 		}
+ 
+		//Array con asientos de Primera Clase
 
 		String[] array = {"1A","2A","3A","4A","1B","2B","3B","4B","1E","2E","3E","4E","1F","2F","3F","4F"};
+ 
+		//Identifica el tipo de asiento que se quiere reservar
 
 		for (int i = 0; i < array.length; i++) {
-			if(numAsiento.equals(array[i])) {
-				PoT = true;
-				break;
-			}
-		}
 
-		if(PoT) {
+			if(numAsiento.toUpperCase().equals(array[i])) {
+
+				isFirstClass = true;
+
+				break;
+
+			}
+
+		}
+ 
+		if(isFirstClass) {
+ 
+			//Si es asiento de Primera Clase, busca el asiento libre y lo cambia por un Asiento Ocupado
 
 			for(int i = 0; i < asientosPrimera.size(); i++) {
-				aux = asientosPrimera.get(i);
-				if(aux.getNumAsiento().equals(numAsiento)) {
-					if(aux.isReservado() != true) { 
+
+				if(asientosPrimera.get(i).getNumAsiento().equals(numAsiento)) {
+
+					if(asientosPrimera.get(i).isReservado() != true) { 
+
 						asientosPrimera.set(i, cambio);
+
+						return;
+
+					}else {
+
+						System.out.println("El asiento esta ocupado. Ingrese otro distinto");
+
 					}
+
 				}
+
 			}
-			
+
 		}else {
 
+			//Si es asiento Turista, busca el asiento libre y lo cambia por un Asiento Ocupado
+
 			for(int i = 0; i < asientosTurista.size(); i++) {
-				aux = asientosTurista.get(i);
-				if(aux.getNumAsiento().equals(numAsiento)) {
-					if(aux.isReservado() != true) { 
+
+				if(asientosTurista.get(i).getNumAsiento().equals(numAsiento)) {
+
+					if(asientosTurista.get(i).isReservado() != true) { 
+
 						asientosTurista.set(i, cambio);
+
+						return;
+
+					}else {
+
+						System.out.println("El asiento esta ocupado. Ingrese otro distinto");
+
 					}
+
 				}
+
 			}
+
 		}
+
 	}
+
 }
--- Avioncito/src/avion/Menu.java
+++ Avioncito/src/avion/Menu.java
@@ -1,196 +1,263 @@
 package avion;
-
-import java.time.LocalDate;
+ 
+import java.util.Iterator;
 import java.util.Scanner;
-
-
-
+ 
 public class Menu implements MainMenuInterface {
-
-	Avion avion = new Avion();
-
+ 
+	public static void main(String[] args) throws AvionException, AsientoException {
+ 
+		Menu menu = new Menu();
+ 
+		menu.iniciar();
+	}
+ 
+	Avion s = new Avion();
+ 
 	@Override
 	public void iniciar() throws AvionException, AsientoException {	
-
-		StringBuffer str = new StringBuffer();
-
-		str.append("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
-		str.append("\n||                              ||");
-		str.append("\n||      ELIJA UNA OPCIÒN:       ||");
-		str.append("\n");
-		str.append("||------------------------------||");
-		str.append("\n||                              ||");
-		str.append("\n");
-		str.append("|| 1 ► Billete Suelto ┊🃏 ");			 
-		str.append("      ||");
-		str.append("\n||                              ||");
-		str.append("\n||                              ||");
-		str.append("\n");
-		str.append("|| 2 ► Varios billetes 🃏┊🃏");  		
-		str.append("     ||");
-		str.append("\n||                              ||");
-		str.append("\n||                              ||");
-		str.append("\n");
-		str.append("|| 3 ► Asiento 👍libre/ocupado⛔ ");  		
-		str.append("||");
-		str.append("\n||                              ||");
-		str.append("\n||                              ||");
-		str.append("\n|| 4 ► ✈️ Mostrar Asientos ✈️");  		
-		str.append("    ||");
-		str.append("\n||                              ||");
-		str.append("\n||                              ||");
-		str.append("\n|| 5 ► Salir 👋    ");  		
-		str.append("             ||");
-		str.append("\n||                              ||");
-		str.append("\n||                              ||");
-		str.append("\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
-		
-		System.out.println(str.toString());
-		int eleccion= scannerInt();
-		if(eleccion==1) {
-			StringBuffer str1 = new StringBuffer();
-
-			str1.append("°°°°°°°°°°°°°°°°°°°°°°°°°°°");
-			str1.append("\n||                       ||");
-			str1.append("\n||   TIPO DE ASIENTO:    ||");
-			str1.append("\n");
-			str1.append("||-----------------------||");
-			str1.append("\n||                       ||");
-			str1.append("\n");
-			str1.append("|| 1 ► Primera Clase");
-			
-			str1.append("     ||");
-			str1.append("\n||                       ||");
-			str1.append("\n||                       ||");
-			str1.append("\n");
-			str1.append("|| 2 ► Clase Turista");  
-			
-			str1.append("     ||");
-			str1.append("\n||                       ||");
-			str1.append("\n||                       ||");
-			str1.append("\n");
-			str1.append("°°°°°°°°°°°°°°°°°°°°°°°°°°°");
-			System.out.println(str1.toString());
-
-			int eleccion11 = scannerInt();
-			if (eleccion11 ==1) {
-				comprarEntrada();
+ 
+ 
+		s.crearAvion();
+		int eleccionOpcion;
+ 
+		do {
+			StringBuffer str = new StringBuffer();
+ 
+			str.append("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+			str.append("\n||                              ||");
+			str.append("\n||      ELIJA UNA OPCIÒN:       ||");
+			str.append("\n");
+			str.append("||------------------------------||");
+			str.append("\n||                              ||");
+			str.append("\n");
+			str.append("|| 1 ► Billete Suelto ┊🃏 ");			 
+			str.append("      ||");
+			str.append("\n||                              ||");
+			str.append("\n||                              ||");
+			str.append("\n");
+			str.append("|| 2 ► Varios billetes 🃏┊🃏");  		
+			str.append("     ||");
+			str.append("\n||                              ||");
+			str.append("\n||                              ||");
+			str.append("\n");
+			str.append("|| 3 ► Asiento 👍libre/ocupado⛔ ");  		
+			str.append("||");
+			str.append("\n||                              ||");
+			str.append("\n||                              ||");
+			str.append("\n|| 4 ► ✈️ Mostrar Asientos ✈️");  		
+			str.append("    ||");
+			str.append("\n||                              ||");
+			str.append("\n||                              ||");
+			str.append("\n|| 5 ► Salir 👋    ");  		
+			str.append("             ||");
+			str.append("\n||                              ||");
+			str.append("\n||                              ||");
+			str.append("\n°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+ 
+			System.out.println(str.toString());
+ 
+			eleccionOpcion = scannerInt();
+ 
+			if(eleccionOpcion == 1 || eleccionOpcion == 2) {
+ 
+				if(eleccionOpcion == 1) {
+ 
+					comprarEntrada();
+ 
+				}
+ 
+				else if(eleccionOpcion == 2) {
+ 
+					comprarEntradas();
+ 
+				}
+ 
 			}
-			if (eleccion11 ==2) {
-				comprarEntradas();
+ 
+			if(eleccionOpcion == 3) {
+ 
 			}
-			else {
-				System.out.println("elija otra opción. La opción elegida no existe.");
+ 
+			if(eleccionOpcion == 4) {
+				s.mostrarAvion();
 			}
 		}
-		if(eleccion==2) {
-			
-			StringBuffer str1 = new StringBuffer();
-
-			str1.append("°°°°°°°°°°°°°°°°°°°°°°°°°°°");
-			str1.append("\n||                       ||");
-			str1.append("\n||   TIPO DE ASIENTO:    ||");
-			str1.append("\n");
-			str1.append("||-----------------------||");
-			str1.append("\n||                       ||");
-			str1.append("\n");
-			str1.append("|| 1 ► Primera Clase");
-			
-			str1.append("     ||");
-			str1.append("\n||                       ||");
-			str1.append("\n||                       ||");
-			str1.append("\n");
-			str1.append("|| 2 ► Clase Turista");  
-			
-			str1.append("     ||");
-			str1.append("\n||                       ||");
-			str1.append("\n||                       ||");
-			str1.append("\n");
-			str1.append("°°°°°°°°°°°°°°°°°°°°°°°°°°°");
-			System.out.println(str1.toString());
-
-			int eleccion12 = scannerInt();
-			if (eleccion12 ==1) {
-				comprarEntrada();
-			}
-			if (eleccion12 ==2) {
-				comprarEntradas();
-			}
-			else {
-				System.out.println("elija otra opción. La opción elegida no existe.");
-			}
-		}
-		if(eleccion==3) {
-			System.out.println("introduzca asiento");
-			
-			comprobarAsiento();
-		}
-
-		if(eleccion==4) {
-			
-			
-			mostrarAsientos();
-		}
-		if(eleccion==5) {
-			System.out.println("Gracias por utilizar nuestra compañia.");
-			return;
-		}
-
-
-		}
-	
-
-
-
+		while(eleccionOpcion != 5);
+ 
+	}
+ 
+ 
+ 
 	@Override
 	public void comprarEntrada() throws AsientoException {
-String vuelo = scanner();
-avion.reservarAsiento(vuelo, null);
-
-		
-		
+ 
+		System.out.println("Estos son los asientos disponibles de primera clase ->");
+ 
+		int separadorPrimera = 0;
+ 
+		for (int i = 0; i < s.asientosPrimera.size(); i++) {
+ 
+			if(separadorPrimera == 4) {
+				System.out.println();
+				separadorPrimera = 0;
+			}
+ 
+			if(!s.asientosPrimera.get(i).reservado) {
+				System.out.print(s.asientosPrimera.get(i).numAsiento + " | ");
+				separadorPrimera++;
+			}
+ 
+		}
+ 
+		System.out.println();
+ 
+		System.out.println("Estos son los asientos disponibles de clase turista ->");
+ 
+		int separadorTurista = 0;
+ 
+		for (int i = 0; i < s.asientosTurista.size(); i++) {
+ 
+			if(separadorTurista == 29) {
+				System.out.println();
+				separadorTurista = 0;
+			}
+ 
+			if(!s.asientosTurista.get(i).reservado) {
+				System.out.print(s.asientosTurista.get(i).numAsiento + " | ");
+				separadorTurista++;
+			}
+ 
+		}
+ 
+		System.out.println();
+ 
+		System.out.println("Dime el asiento que quieres reservar.");
+		String vuelo = scannerString().toUpperCase();
+		System.out.println("Dime tus datos: ");
+		System.out.println("DNI: ");
+		String dni = scannerString();
+		System.out.println("Nombre: ");
+		String nombre = scannerString();
+		System.out.println("Apellido: ");
+		String apellido = scannerString();
+		System.out.println("Edad: ");
+		int edad = scannerInt();
+		Persona p = new Persona(dni, nombre, apellido, edad);
+		s.reservarAsiento(vuelo, p);
+		System.out.println("De parte de David, te agradecemos la compra");
+ 
+ 
 	}
-
+ 
 	@Override
 	public void comprarEntradas() throws AsientoException {
-		String vuelos = scanner();
-		avion.reservarAsiento(vuelos, null);
-		
-		
+ 
+		System.out.println("¿Cuantas billetes quieres?");
+		int numDeBilletes = scannerInt();
+		String vuelos = "";
+ 
+		if(numDeBilletes < 1) {
+			System.out.println("Usted es imbecil, introduzca un numero valido, y deje de hacernos perder el tiempo");
+			numDeBilletes = scannerInt();
+		}
+		if(numDeBilletes == 1) {
+			System.out.println("Usted es imbecil, para la proxima elija la opcion uno en el menu anterior");
+			comprarEntrada();
+		}
+		if(numDeBilletes > 1) {
+ 
+ 
+			System.out.println("Dime tus datos: ");
+			System.out.println("DNI: ");
+			String dni = scannerString();
+			System.out.println("Nombre: ");
+			String nombre = scannerString();
+			System.out.println("Apellido: ");
+			String apellido = scannerString();
+			System.out.println("Edad: ");
+			int edad = scannerInt();
+			Persona p = new Persona(dni, nombre, apellido, edad);
+ 
+			System.out.println("Opcion 1-> Primera clase \nOpcion 2-> Clase turista");
+			int numOpcion = scannerInt();
+ 
+			int disponibles = 0;
+ 
+			if(numOpcion == 1) {
+ 
+				for (int i = 0; i < s.asientosPrimera.size(); i++) {
+					if(!s.asientosPrimera.get(i).reservado) {
+						disponibles++;
+					}
+				}
+ 
+ 
+				if(disponibles <= numDeBilletes) {
+ 
+					int comprados = 0;
+ 
+					if(numDeBilletes%2 == 0) {
+						for (int i = 0; i < s.asientosPrimera.size(); i+=2) {
+							if(comprados != numDeBilletes) {
+								if(!s.asientosPrimera.get(i).reservado && !s.asientosPrimera.get(i+1).reservado) {
+									s.reservarAsiento(s.asientosPrimera.get(i).numAsiento, p);
+									s.reservarAsiento(s.asientosPrimera.get(i+1).numAsiento, p);
+									comprados+=2;
+								}
+							}
+						}
+					}
+				}
+			}
+ 
+			for (int i = 0; i < s.avionAsientos.size(); i++) {
+ 
+			}
+ 
+			for (int i = 1; i < numDeBilletes; i++) {
+ 
+ 
+ 
+ 
+				s.reservarAsiento(vuelos, p);
+			}
+		}
 	}
-
+ 
 	@Override
-	public void comprobarAsiento() {
-		String asiento=scanner();
-		avion.comprobarAsiento(asiento);
-		
-		
+	public void comprobarAsiento() throws AsientoException {
+ 
+		String asiento=scannerString();
+		s.comprobarAsiento(asiento);
+ 
 	}
-
+ 
 	@Override
 	public void mostrarAsientos() throws AvionException {
-		avion.mostrarAvion();
-
+ 
+		s.mostrarAvion();
+ 
 	}
-	public String scanner() {
-
+ 
+	//NO tocar
+	public String scannerString() {
+ 
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		String n = sc.nextLine();
-
+ 
 		return n;
-
+ 
 	}
 	public int scannerInt() {
-
+ 
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-
+ 
 		return n;
-
+ 
 	}
-	
-	
-
+ 
 }
